989d0f7fe16802d9134a06672af7336c6037367d
Imran-hossain897 <imran.hossain984321@gmail.com>
2020-07-17 21:09:09 +0600

    finish javascript

--- assignment.js
+++ assignment.js
@@ -6,6 +6,8 @@ var mile = feetToMile(10562);
 console.log(mile);
 
 
+
+
 function woodCalculator(chair, table, bed){
     chairCount = 1 * chair;
     tableCount = 3 * table;
@@ -15,6 +17,33 @@ function woodCalculator(chair, table, bed){
 }
 var totalCount = woodCalculator(1, 0, 4);
 console.log(totalCount);
+
+
+
+function brickCalculator(n){
+    if(n<=10){
+        var  level1 = n*15;
+        var brickForLevel1 = level1*1000;
+        return brickForLevel1;
+    }
+   if(n<=20){
+        var level2 = n*12;
+        var brickForLevel2 = level2*1000;
+        var total = brickForLevel2+30000;
+        return total;
+    }
+     else{
+        var level3 = n*10;
+        var brickForLevel3 = level3*1000;
+        var amount = brickForLevel3+70000;
+        return amount;
+    }
+}
+var result = brickCalculator(1);
+console.log(result);
+
+
+
 
 
 function tinyFriend(string) {
